fcc54ef4e051739ae637d8566bda3e56b5fd67ac
baronjada <jadachantellebaron@gmail.com>
2024-04-18 18:36:00 +0100

    Built updateTime function to update London and LA time every second

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
-// LONDON
-let londonElement = document.querySelector("#london");
-let londonDateElement = document.querySelector(".date");
-let londonTimeElement = document.querySelector(".time");
-let londonDateAndTime = moment();
+function updateTime() {
+  // London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonDateAndTime = moment().tz("Europe/London");
 
-londonDateElement.innerHTML = londonDateAndTime.format("MMMM Do YYYY");
-londonTimeElement.innerHTML = `${londonDateAndTime.format(
-  "h:mm:ss"
-)} <small>${londonDateAndTime.format("A")}</small>`;
+  londonDateElement.innerHTML = londonDateAndTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonDateAndTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesDateAndTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML =
+    losAngelesDateAndTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesDateAndTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
